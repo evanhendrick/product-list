9e05edf12d9547deb3d490e373882db717f9f487
Evan Hendrick <evanjh1996@gmail.com>
2025-02-03 23:34:29 -0500

    Filters for GET /products

--- routes/main.js
+++ routes/main.js
@@ -11,38 +11,127 @@ router.get("/generate-fake-data", (req, res, next) => {
     product.name = faker.commerce.productName();
     product.price = faker.commerce.price();
     product.image = "https://via.placeholder.com/250?text=Product+Image";
+    product.reviews = []
 
     product.save((err) => {
       if (err) throw err;
     });
   }
-  res.end();
+  res.send("Fake data generated.");
 });
 
 router.get("/products", (request, response, next) => {
-  const perPage = 9;
 
+  const perPage = 9;
   const page = request.query.page || 1;
 
-  Product.find({})
+  const category = request.query.category
+  const priceHigh = request.query.priceHigh
+  const priceLow = request.query.priceLow
+  const string = request.query.string
+
+  if(category && priceHigh && string){
+    Product.find({$and: [{category: category}, {name: {$regex: string, $options: "i"}}]})
+    .sort({price: -1})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .then((products) => {
+      response.send(products)
+    })
+  } else if(category && priceLow && string){
+    Product.find({$and: [{category: category}, {name: {$regex: string, $options: "i"}}]})
+    .sort({price: 1})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .then((products) => {
+      response.send(products)
+    })
+  } else if(category && priceHigh){
+    Product.find({ category: category})
+    .sort({price: -1})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .then((products) => {
+      response.send(products)
+    })
+  } else if(category && priceLow){
+    Product.find({category: category})
+    .sort({price: 1})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .then((products) => {
+      response.send(products)
+    })
+  } else if(category && string) {
+    Product.find({$and: [{category: category},{name: {$regex: string, $options: "i"}}]})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .then((products) => {
+      response.send(products)
+    })
+  } else if(string && priceHigh){
+    Product.find({name: {$regex: string, $options: "i"}})
+    .sort({price: -1})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .then((products) => {
+      response.send(products)
+    })
+  } else if(string && priceLow){
+    Product.find({name: {$regex: string, $options: "i"}})
+    .sort({price: 1})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .then((products) => {
+      response.send(products)
+    })
+  } else if (priceHigh && priceLow){
+    // The better way would just be to prevent the user from being able to do this on the front end to begin with. But we'll keep this as an extra barrier
+    response.status(400).send("Error Code 400: Bad request! Conflicting filters set.")
+  } else if(string){
+    console.log(string)
+    Product.find({name: {$regex: string, $options: 'i'}})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .then((products) => {
+      response.send(products)
+    })
+  } else if(category){
+    Product.find({category: category})
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .then((products) => {
+      response.send(products)
+    })
+  } else if(priceHigh){
+    Product.find()
+    .sort({price: -1})
+    .skip(perPage*page-perPage)
+    .limit(perPage)
+    .exec((err, products) => {
+      response.send(products)
+    })
+  } else if(priceLow){
+    Product.find()
+    .sort({price:1})
     .skip(perPage * page - perPage)
     .limit(perPage)
     .exec((err, products) => {
-      Product.count().exec((err, count) => {
-        if (err) return next(err)
-
-        response.send(products)
-      })
+      response.send(products)
     })
+  } else {
+    Product.find()
+    .skip(perPage *page - perPage)
+    .limit(perPage)
+    .exec((err, products) => {
+      response.send(products)
+    })
+  }
 });
 
-// returns a specific product by its ID
-// [CHECK!]
 router.get("/products/:product", (request, response) => {
-  // console.log("REQUEST PARAMS>>", request.params)
+  
   let prodId = request.params.product
-
-  console.log("PRODUCT ID>>", prodId)
 
   Product.find({_id: prodId})
   .then((prod) => {
@@ -54,8 +143,6 @@ router.get("/products/:product", (request, response) => {
   })
 })
 
-// returns the reviews of a specific product
-// [CHECK!]
 router.get("/products/:product/reviews", (request, response, next) => {
   let prodId = request.params.product
 
@@ -65,8 +152,6 @@ router.get("/products/:product/reviews", (request, response, next) => {
   })
 })
 
-// creates a new product to the DB
-// [CHECK!]
 router.post("/products", (request, response, next) => {
   let params = request.body
 
@@ -77,46 +162,85 @@ router.post("/products", (request, response, next) => {
   product.price = params.price
   product.image = params.image
 
-  // product.save((err) => {
-  //   if (err) throw err
-  // })
-
-  console.log("NEW PRODUCT>>", product)
-  response.end()
+  response.send("New product added!")
 })
 
-// creates a new review in the DB by adding it to the correct product's reviews array
-//needs to find the matching product and save the new review to that product's review array
-router.post("/products/:product/reviews", async (request, response, next) => {
+// router.post("/products/:product/reviews", async (request, response, next) => {
 
-  console.log("REQUEST BODY>>", request.body)
+//   console.log("REQUEST BODY>>", request.body)
 
-  let review = new Review({
-    username: request.body.userName,
-    text: request.body.text,
-    product: request.body.product
-  })
+//   let review = new Review({
+//     username: request.body.userName,
+//     text: request.body.text,
+//     product: request.body.product
+//   })
 
-  console.log("REVIEW PRODUCT ID>>", review.product)
+//   console.log("REVIEW PRODUCT ID>>", review.product)
   
-  console.log("NEW REVIEW>>", review)
-  await review.save((err) => {
-    if(err) throw err
-  })
+//   console.log("NEW REVIEW>>", review)
+//   await review.save((err) => {
+//     if(err) throw err
+//   })
 
-  let newProduct = Product.findOne({ _id: request.body.product })
-  // .exec((err, prod) =>{
-  //   prod.reviews.push(review._id)
+//   let newProduct = Product.findOne({ _id: request.body.product })
+//   // .exec((err, prod) =>{
+//   //   prod.reviews.push(review._id)
+//   // })
+
+//   newProduct.reviews.push(review._id)
+//   console.log("NEW PRODUCT", newProduct)
+
+//   await newProduct.save((err) => {
+//     if(err) throw err
+//   })
+
+//   response.end()
+// })
+
+//goal of endpoint is to find the product, create the new review, and save the review to that found product's reviews array
+
+router.post("/products/:product/reviews", async (request, response) => {
+  const params = request.params
+  const reqBody = request.body
+  // console.log("REQUEST BODY>>", reqBody)
+
+  // we need to instantiate a new review object>> as in a reference to the review object model!
+  let review = new Review()
+
+  review.username = request.body.userName
+  review.text = request.body.text
+  review.product = request.body.product
+
+  review.save()
+
+  console.log("NEW REVIEW>>", review)
+
+  //review is created by the contents of request.body
+
+  //ERROR product.save() is not a function...? >> try assigning product found to a new variable
+  // await Product.find({_id: params.product})
+  // .then((product) => {
+  //   console.log("PRODUCT FOUND >>", product[0].reviews)
+  //   product[0].reviews.push(review)
+  //   product.save()
+  //   console.log(product)
   // })
 
-  newProduct.reviews.push(review._id)
-  console.log("NEW PRODUCT", newProduct)
-
-  await newProduct.save((err) => {
-    if(err) throw err
+  let productFound = Product.find({_id: request.body.product})
+  .then((product) => {
+    console.log("PRODUCT FOUND>>", product)
+    // product[0].reviews.push(review)
+    return product
   })
+  // await productFound[0].reviews.push(review)
 
-  response.end()
+  console.log("PRODUCT FOUND REVIEW >>", productFound)
+  // await productFound[0].save()
+
+
+  response.send("New review created!")
 })
+
+//'DELETE' ENDPOINTS
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-//connection string copied from MongoDB Compass
-// mongodb://localhost:27017/
-
-//this is an asynchronous function and returns a promise. It takes a minute to respond
-//needed 127.0.0.1 to fix connection issue
-// and needed to specify which DB we were connecting to in order to write to it (i.e. /myapp)
-
-//mongodb://127.0.0.1:27017/products
 mongoose.connect("mongodb://127.0.0.1/products", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -37,6 +29,3 @@ app.use(mainRoutes);
 app.listen(8000, () => {
   console.log("Node.js listening on port " + 8000);
 });
-
-//1/23/25: we are not making our connection properly
-//1. connect on atlas DB 2. run node server.js 3. try to figure out what the issue is with connecting to your DB
